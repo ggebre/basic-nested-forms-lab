2fd71adc2b0e28a6382815a43d868281d8139ad9
Getu Gebre <getu.gebre.gg@gmail.com>
2020-12-02 21:41:30 -0600

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,11 +6,26 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
-
+  
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'rice')
+    @recipe.ingredients.build(name: 'rice')
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private 
+  def recipe_params 
+    params.require(:recipe).permit(
+      :title,
+    ingredients_attributes: [
+      :name,
+      :quantity
+    ]
+    )
   end
 end
